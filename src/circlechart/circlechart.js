e26bfca73cbc74012330562c705faf4974ed19f1
maobonhaha <maobonhaha@gmail.com>
2024-07-04 17:12:32 +0800

    second commit

--- src/circlechart/circlechart.js
+++ src/circlechart/circlechart.js
@@ -2,6 +2,6 @@ import * as echarts from 'echarts';
 import option from "./basic";
 
 // 基于准备好的dom，初始化echarts实例
-const myChart = echarts.init(document.getElementById('main'));
+const myChart = echarts.init(document.getElementById('main2'));
 myChart.setOption(option)
 
